1d6922ba065834ef8e9feb963a8b1bb20c8552b6
aqeelakhan <redda012@newschool.edu>
2017-04-20 21:03:39 -0400

    Added javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("linked"); 
+
+$(function(){
+  $('button').click(function(){
+   window.location.href=('http://cdn8.whiskeyriff.com/wp-content/uploads/rexfeatures_372078p.jpg');
+});
+
+});
